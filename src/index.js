bedac3074664b4995937d049bfd55a2e176369e0
Kseniya Belyavtseva <kseniyabelyavtseva@protonmail.com>
2020-09-17 20:54:42 +0300

    feat: solve towe sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined) {
+    return [];
+  }
+  let i = 1;
+  while (i < matrix.length) {
+    matrix[i] = matrix[i].reverse();
+    i += 2;
+  }
+  return matrix.flat();
 }
+
+// console.log(towelSort([ [1, 2, 3], [4, 5, 6]]))
+// console.log(towelSort())
+// console.log(towelSort())
+// console.log(towelSort())
